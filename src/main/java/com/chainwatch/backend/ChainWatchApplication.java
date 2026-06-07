f262c216aae406afa036a43e1c12ce294e9eaff5
bitofsyn <soyounjeong6276@gmail.com>
2026-06-07 18:47:24 +0900

    init

---
+++ src/main/java/com/chainwatch/backend/ChainWatchApplication.java
@@ -0,0 +1,12 @@
+package com.chainwatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainWatchApplication.class, args);
+    }
+}
